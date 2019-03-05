bc248360b557a092f809d39233cb3f9b92f9ec5d
Jesús Urrutia <jurrutia@acid.cl>
2019-03-05 14:08:51 -0300

    feat: added header component

---
+++ components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <nuxt-link class="navbar-brand" to="/">
+        <img src="~/assets/images/logo.png" width="150">
+      </nuxt-link>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <nuxt-link class="nav-link" to="/">Inicio</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/nosotros">Nosotros</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/universidades">Universidades</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/descargas">Descargas</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
