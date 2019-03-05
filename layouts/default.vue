607424391aa95c34142368ffa41f40586ba44fa5
Jesús Urrutia <jurrutia@acid.cl>
2019-03-05 14:09:20 -0300

    feat: include header in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
+    <app-header></app-header>
+
     <nuxt/>
   </div>
 </template>
+
+<script>
+import Header from '~/components/Header';
+
+export default {
+  components: {
+    AppHeader: Header
+  }
+};
+</script>
 
 <style lang="sass">
 @import '../assets/sass/custom.scss'
