476f9fee8b8d362b19942f89336659515601e126
Jesús Urrutia <jurrutia@acid.cl>
2019-03-05 14:09:42 -0300

    feat: added some pages

--- pages/nosotros.vue
+++ pages/nosotros.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="container">
+  <div class="container">
     <div class="row">
       <div class="col-12">
-        <h1 class="h1">Inicio</h1>
+        <h1 class="h1">Nosotros</h1>
         <hr>
       </div>
     </div>
-  </section>
+  </div>
 </template>
