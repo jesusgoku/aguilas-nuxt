4f51217397cf1871578508c9eb73c57db429e093
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-03-16 03:06:43 -0300

    feat: migrate navbar to bootstrap-vue components

--- components/Header.vue
+++ components/Header.vue
@@ -1,45 +1,43 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <nuxt-link class="navbar-brand" to="/">
+  <b-navbar toggleable="lg" type="light" variant="light">
+    <b-container>
+      <b-navbar-brand to="/">
         <img src="~/assets/images/logo.png" width="150">
-      </nuxt-link>
+      </b-navbar-brand>
 
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon" />
-      </button>
-      <div id="navbarNav" class="collapse navbar-collapse">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <nuxt-link class="nav-link" to="/">
-              Inicio
-            </nuxt-link>
-          </li>
-          <li class="nav-item">
-            <nuxt-link class="nav-link" to="/nosotros">
-              Nosotros
-            </nuxt-link>
-          </li>
-          <li class="nav-item">
-            <nuxt-link class="nav-link" to="/universidades">
+      <b-navbar-toggle target="nav_collapse" />
+
+      <b-collapse id="nav_collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/">
+            Inicio
+          </b-nav-item>
+
+          <b-nav-item-dropdown text="Nosotros" right>
+            <b-dropdown-item to="/nosotros/universidades">
               Universidades
-            </nuxt-link>
-          </li>
-          <li class="nav-item">
-            <nuxt-link class="nav-link" to="/descargas">
-              Descargas
-            </nuxt-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+            </b-dropdown-item>
+            <b-dropdown-item to="/nosotros/ciudades">
+              Ciudades
+            </b-dropdown-item>
+            <b-dropdown-item to="/nosotros/naciones">
+              Naciones
+            </b-dropdown-item>
+            <b-dropdown-item to="/nosotros/sociedad">
+              Sociedad
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item to="/universidades">
+            Universidades
+          </b-nav-item>
+
+          <b-nav-item to="/descargas">
+            Descargas
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
 </template>
